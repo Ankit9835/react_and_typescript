5dd0e7bcbd9aea30d21a47f54afe5664ec98717e
Ankit9835 <ankydchrismatic@gmail.com>
2024-07-10 21:46:23 +0530

    working on react typescript basic

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
-import Component from "./starter/01-return";
+import Component from "./starter/02-props";
 
 function App() {
   return (
     <main>
-      <Component />
+      <Component name='test' id={123} >
+        content
+      </Component>
+      <Component name='test' id={123} >
+      </Component>
     </main>
   );
 }
