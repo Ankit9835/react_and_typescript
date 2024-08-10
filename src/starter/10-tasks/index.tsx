28b8483aa54083b4f4657ce3be7550e6cb6aed03
Ankit9835 <ankydchrismatic@gmail.com>
2024-08-10 22:02:20 +0530

    react with typescript basic completed

--- src/starter/10-tasks/index.tsx
+++ src/starter/10-tasks/index.tsx
@@ -1,60 +1,30 @@
-import { useEffect, useState } from "react";
-import { type Tour, tourSchema } from './types';
-const url = 'https://www.course-api.com/react-tours-project';
+import React, { useEffect, useState } from 'react'
+import Form from './Form'
+import List from './List'
+import { type Task } from './types'
 
-
-function Component() {
-
-  const [isLoading,setLoading] = useState<boolean>(false)
-  const [isError, setError] = useState<string | null>(null)
-  const [tours, setTours] = useState<Tour[]>([])
-
-  const fetchData = async () => {
-    try {
-      const response = await fetch(url)
-      if(!response.ok){
-        throw new Error('Failed to fetch...')
-      }
-
-      const rawData: Tour[] = await response.json()
-      const result = tourSchema.array().safeParse(rawData);
-
-      if (!result.success) {
-        console.log(result.error.message);
-        throw new Error(`Failed to parse tours`);
-      }
-      setTours(result.data);
-    } catch (error) {
-      const message = error instanceof Error ? error.message : 'There was an Error'
-      setError(message)
-    } finally{
-      setLoading(false)
-    }
+const Component = () => {
+  const  [tasks,setTask] = useState<Task[]>([])
+  const addTask = (task:Task) => {
+    setTask([...tasks,task])
   }
 
-  useEffect(() => {
-    fetchData()
-  },[])
-
-  if(isLoading){
-    return <h3>is Loading</h3>
+  const toggleTask = ({id}: {id:string}) => {
+    setTask(
+      tasks.map((task) => {
+        if(task.id === id){
+          return {...task, isCompleted: !task.isCompleted}
+        }
+        return task
+      })
+    )
   }
-
-  if(isError){
-    return <h3>is Error</h3>
-  }
-
   return (
-    <div>
-      <h2>React & Typescript</h2>
-      {tours.map((tour) => {
-        return (
-          <p key={tour.id} className='mb-1'>
-            {tour.name}
-          </p>
-        );
-      })}
-    </div>
-  );
+    <section>
+      <Form addTask={addTask}/>
+      <List tasks={tasks} toggleTask={toggleTask}/>
+    </section>
+  )
 }
-export default Component;
+
+export default Component
